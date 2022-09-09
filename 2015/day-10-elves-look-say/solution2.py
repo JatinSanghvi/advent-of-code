154ee3afe6bed4a2ca42c1235f6d70f4de3a7b79
Jatin Sanghvi <20547963+JatinSanghvi@users.noreply.github.com>
2022-09-09 11:47:16 +0530

    Add solution for 2015 Day 10

---
+++ 2015/day-10-elves-look-say/solution2.py
@@ -0,0 +1,24 @@
+def solve(value, steps):
+    for _ in range(steps):
+        new_value = ''
+        char = value[0]
+        copies = 1
+        for next_char in value[1:] + '$':
+            if char == next_char:
+                copies += 1
+            else:
+                new_value += str(copies) + char
+                char = next_char
+                copies = 1
+        value = new_value
+
+    print(len(value))
+
+
+def main():
+    solve('1', 5)
+    solve('3113322113', 50)
+
+
+if __name__ == '__main__':
+    main()
