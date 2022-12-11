2bcae99ecf9e5e2a9b07620eead446c16d92ccae
Jatin Sanghvi <20547963+JatinSanghvi@users.noreply.github.com>
2022-12-11 13:17:58 +0530

    Add solution for 2022 Day 11

---
+++ 2022/day-11-monkey-in-the-middle/solution1.py
@@ -0,0 +1,52 @@
+from typing import Callable, List
+
+
+class Monkey:
+    def __init__(self, items: List[int], op: Callable[[int], int], divisor: int, next_true: int, next_false: int) -> None:
+        self.items = items
+        self.op = op
+        self.divisor = divisor
+        self.next_true = next_true
+        self.next_false = next_false
+        self.inspections = 0
+
+
+def solve(path):
+    with open(path) as file:
+        lines = [line.rstrip('\n') for line in file.readlines()]
+
+    monkeys: List[Monkey] = []
+
+    for line_index in range(0, len(lines), 7):
+        monkeys.append(Monkey(
+            items=eval('[' + lines[line_index + 1].removeprefix('  Starting items: ') + ']'),
+            op=eval('lambda old: ' + lines[line_index + 2].removeprefix('  Operation: new = ')),
+            divisor=int(lines[line_index + 3].removeprefix('  Test: divisible by ')),
+            next_true=int(lines[line_index + 4].removeprefix('    If true: throw to monkey ')),
+            next_false=int(lines[line_index + 5].removeprefix('    If false: throw to monkey ')),
+        ))
+
+    for _ in range(20):
+        for monkey in monkeys:
+            for item in monkey.items:
+                monkey.inspections += 1
+                worry = monkey.op(item) // 3
+
+                if worry % monkey.divisor == 0:
+                    monkeys[monkey.next_true].items.append(worry)
+                else:
+                    monkeys[monkey.next_false].items.append(worry)
+
+            monkey.items = []
+
+    inspections = sorted(monkey.inspections for monkey in monkeys)
+    print(inspections[-1] * inspections[-2])
+
+
+def main():
+    solve('input-test.txt')  # 10605
+    solve('input.txt')  # 64032
+
+
+if __name__ == '__main__':
+    main()
