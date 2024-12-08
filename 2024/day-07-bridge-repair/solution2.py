30b46157d5cce4263f7bbd5a3545af6e19127eba
Jatin Sanghvi <20547963+JatinSanghvi@users.noreply.github.com>
2024-12-08 12:20:16 +0530

    Add solution for 2024 Day 07

---
+++ 2024/day-07-bridge-repair/solution2.py
@@ -0,0 +1,26 @@
+def solve(path: str) -> None:
+    with open(path, encoding="utf-8") as file:
+        lines = [line.rstrip("\n") for line in file.readlines()]
+
+    total = 0
+    for line in lines:
+        result = int(line.split(": ")[0])
+        operands = list(map(int, line.split(": ")[1].split(" ")))
+
+        results = set([operands[0]])
+        for op in operands[1:]:
+            new_results = set()
+            for res in results:
+                new_results.add(res + op)
+                new_results.add(res * op)
+                new_results.add(int(str(res) + str(op)))
+            results = new_results
+
+        if result in results:
+            total += result
+
+    print(total)
+
+
+solve("input-test.txt")  # 3749
+solve("input.txt")  # 1153997401072
